2fd12c6370aadaf7b46ee41c996b95894e3f8dd5
Josvalk <josvalk775gmail.com>
2021-07-15 10:39:00 -0500

    Cambio en el nav, ssaludo y habilidades

---
+++ js/escritura.js
@@ -0,0 +1,16 @@
+var typed1 = new Typed('.typed1', {
+    strings: ['¡Hola Mundo!'],
+    typeSpeed: 60
+});
+
+var typed = new Typed('.typed', {
+    strings: ['Soy Joseph Valbuena'],
+    typeSpeed: 60,
+    startDelay: 1000
+});
+
+var typed2 = new Typed('.typed2', {
+    strings: ['Desarrollador Web Front-end Junior'],
+    typeSpeed: 40,
+    startDelay: 2800
+});
--- js/index.js
+++ js/index.js
@@ -1,4 +1,40 @@
 $(document).ready(function() {
+
+    //Deteccion de la posición del scroll
+
+    $(document).on('scroll', function() {
+        var sv = $(document).scrollTop();
+        // console.log(sv);
+
+        if (sv >= 0 && sv < 557) {
+            $(".saludo").addClass("active");
+            $(".habilidades").removeClass("active");
+            $(".proyectos").removeClass("active");
+            $(".contact1").removeClass("active");
+        } else if (sv >= 558 && sv < 1349) {
+            $(".saludo").removeClass("active");
+            $(".habilidades").addClass("active");
+            $(".proyectos").removeClass("active");
+            $(".contact1").removeClass("active");
+        } else if (sv >= 1350 && sv < 2619) {
+            $(".saludo").removeClass("active");
+            $(".habilidades").removeClass("active");
+            $(".proyectos").addClass("active");
+            $(".contact1").removeClass("active");
+        } else {
+            $(".saludo").removeClass("active");
+            $(".habilidades").removeClass("active");
+            $(".proyectos").removeClass("active");
+            $(".contact1").addClass("active");
+        }
+    })
+
+    // --------- Botones Barra de navecacioón ---------
+
+
+
+    $(".saludo").addClass("active");
+
     $(".saludo").click(function() {
         $(".saludo").addClass("active");
         $(".habilidades").removeClass("active");
@@ -25,5 +61,40 @@ $(document).ready(function() {
         $(".habilidades").removeClass("active");
         $(".proyectos").removeClass("active");
         $(".contact1").addClass("active");
+    });
+
+    // --------- Botones y habilidades ---------
+
+    $(".buttonFront").addClass("active");
+    $(".front").css("display", "block");
+    $(".back").css("display", "none");
+    $(".langs").css("display", "none");
+
+
+    $(".buttonFront").click(function() {
+        $(".buttonFront").addClass("active");
+        $(".front").css("display", "block");
+        $(".buttonBack").removeClass("active");
+        $(".back").css("display", "none");
+        $(".buttonLangs").removeClass("active");
+        $(".langs").css("display", "none");
+    });
+
+    $(".buttonBack").click(function() {
+        $(".buttonFront").removeClass("active");
+        $(".front").css("display", "none");
+        $(".buttonBack").addClass("active");
+        $(".back").css("display", "block");
+        $(".buttonLangs").removeClass("active");
+        $(".langs").css("display", "none");
+    });
+
+    $(".buttonLangs").click(function() {
+        $(".buttonFront").removeClass("active");
+        $(".front").css("display", "none");
+        $(".buttonBack").removeClass("active");
+        $(".back").css("display", "none");
+        $(".buttonLangs").addClass("active");
+        $(".langs").css("display", "block");
     });
 });
